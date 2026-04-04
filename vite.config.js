6105ec705fb2a2fab35fde9ab8ce02ab0202a377
Prateek Rathod <emaillawrathod@gmail.com>
2026-04-04 04:27:10 +0200

    fix: try new
    Signed-off-by: Prateek Rathod <emaillawrathod@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/bgrem/',
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
-  preview: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
   build: {
+    outDir: 'dist/bgrem',
     rollupOptions: {
       output: {
         manualChunks: {
